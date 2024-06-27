8ea96e2f5cfc70c263af09d5262c55090035237e
yunho1017 <yunho101775@gmail.com>
2024-06-27 17:26:10 +0900

    fix: esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 const babel = require("@rollup/plugin-babel").default;
 const commonjs = require("@rollup/plugin-commonjs");
 const resolve = require("@rollup/plugin-node-resolve").default;
+const path = require("path");
 
 module.exports = [
   {
@@ -9,6 +10,8 @@ module.exports = [
       dir: "esm",
       format: "es",
       entryFileNames: "[name].mjs",
+      preserveModules: true,
+      preserveModulesRoot: path.dirname("src/index.ts"),
     },
     plugins: [
       resolve({ extensions: [".js", ".jsx", ".ts", ".tsx"] }),
